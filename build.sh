955371a1816a4b327249e7b973fd9185c2913fd1
Markus Kobler <markus@distinctive.co>
2016-10-02 12:19:52 +0100

    Updating build

--- build.sh
+++ build.sh
@@ -1,25 +1,30 @@
 #!/usr/bin/env bash
-set -eux
+#
+# Download and build glibc
+set -euo pipefail
 
-GLIBC_VERSION=2.24
-GLIBC_SHA256SUM=99d4a3e8efd144d71488e478f62587578c0f4e1fa0b4eed47ee3d4975ebeb5d3
+src=$PWD/glibc
+out=$PWD/out
 
-SRC=$PWD/src
-OUT=$PWD/out
+# _download "version" "sha256"
+_download() {
+  mkdir -p ${src}/build
+  curl -OL http://ftp.gnu.org/gnu/libc/glibc-${1}.tar.xz
+  echo "${2}  glibc-${1}.tar.xz" | sha256sum -c
+  tar -C ${src} --strip-components=1 -xf glibc-${1}.tar.xz
+}
 
-mkdir -p $SRC/build $OUT
-
-curl -OL http://ftp.gnu.org/gnu/libc/glibc-$GLIBC_VERSION.tar.xz
-echo "$GLIBC_SHA256SUM  glibc-$GLIBC_VERSION.tar.xz" | sha256sum -c
-tar -C $SRC --strip-components=1 -xf glibc-$GLIBC_VERSION.tar.xz
-
-cd $SRC/build
-../configure \
+_build() {
+  cd ${src}/build
+  ../configure \
     --enable-shared \
     --without-cvs \
     --without-gd \
     --disable-profile \
     --disable-sanity-checks
-make
-
-mv $SRC/build/* $OUT
+  make
+  mv ${src}/build/* $out
+}
+  
+_download 2.24 99d4a3e8efd144d71488e478f62587578c0f4e1fa0b4eed47ee3d4975ebeb5d3
+_build
